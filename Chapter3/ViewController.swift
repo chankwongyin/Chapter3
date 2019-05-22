4492a33ec3e7bb2e55323836cf0a958dc3bf5ae4
chankwongyin <chankwongyintony@gmail.com>
2019-05-23 04:34:40 +0800

    add all the learning notes and trials

--- Chapter3/ViewController.swift
+++ Chapter3/ViewController.swift
@@ -10,11 +10,55 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //MARK: optional
+        
+        //can set nil to the variable
+        
+        struct Human {
+            var name : String?
+        }
+        let tony = Human(name: "Tony")
+        let noOne = Human(name: nil) //error if without ? after String
+        
+        
+        //optional binding
+        if let name = tony.name {
+            print(name)
+        }
+        
+        
+        //MARK: Type casting and Inspection
+        
+        //use as? to downcast the type in a subclass
+        class Animal{
+            
+        }
+        class Cat: Animal{
+            
+        }
+        class Dog: Animal{
+            
+        }
+//        let pet = Cat()
+        let pet = Dog()
+        if let cat = pet as? Cat {
+            print("it is cat")
+        }
+        else if let dog = pet as? Dog {
+            print("it is dog")
+        }
     }
 
-
+    @IBAction func unwindToRed(unwindSegue: UIStoryboardSegue) {
+        
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        segue.destination.navigationItem.title = textField.text
+    }
 }
 
